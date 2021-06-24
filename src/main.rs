57ff2745ccf70151a225ce2aefebb3310b9f2120
Vincent Auguin <vauguin@ippon.fr>
2021-06-25 01:56:30 +0200

    feat: create a function which count the number of points in the inscribing circle

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,37 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn generate_random_number() -> f64 {
-    let mut rng = rand::thread_rng();
-    let seeder = Uniform::from(LOWER_INCLUSIVE_BOUND..UPPER_INCLUSIVE_BOUND);
-    return seeder.sample(&mut rng);
+fn count_points_in_circle(points: Vec<Point2>) -> usize {
+    return points
+        .iter()
+        .filter(|p| p.x.powi(2) + p.y.powi(2) < 1.0)
+        .count();
+}
+
+fn generate_points(n: i32) -> Vec<Point2> {
+    let mut points = Vec::new();
+    let mut i = 0;
+    while i < n {
+        points.push(generate_point());
+        i = i + 1;
+    }
+    return points;
 }
 
 fn generate_point() -> Point2 {
     return Point2 { x: generate_random_number(), y: generate_random_number() };
 }
 
+fn generate_random_number() -> f64 {
+    let mut rng = rand::thread_rng();
+    let seeder = Uniform::from(LOWER_INCLUSIVE_BOUND..UPPER_INCLUSIVE_BOUND);
+    return seeder.sample(&mut rng);
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{generate_point, generate_random_number};
+    use crate::{count_points_in_circle, generate_point, generate_points, generate_random_number};
+    use crate::point::Point2;
 
     #[test]
     fn it_returns_number_between_0_and_1() {
@@ -46,5 +64,28 @@ mod tests {
         let point = generate_point();
         assert!(point.x >= 0.0 && point.x <= 1.0);
         assert!(point.y >= 0.0 && point.y <= 1.0);
+    }
+
+
+    #[test]
+    fn it_returns_list_of_points() {
+        let points = generate_points(10);
+        assert_eq!(points.len(), 10);
+        let points = generate_points(20);
+        assert_eq!(points.len(), 20);
+    }
+
+    #[test]
+    fn it_returns_number_of_point_in_circle_of_radius_1() {
+        let points_in_circle = count_points_in_circle(Vec::new());
+        assert_eq!(points_in_circle, 0);
+
+        let mut points = Vec::new();
+        points.push(Point2 { x: 0.0, y: 0.0 });     // INNER
+        points.push(Point2 { x: 0.25, y: 0.1 });    // INNER
+        points.push(Point2 { x: 0.1, y: 0.1 });     // INNER
+        points.push(Point2 { x: 0.99, y: 0.99 });   // OUTER
+        let points_in_circle = count_points_in_circle(points);
+        assert_eq!(points_in_circle, 3);
     }
 }
